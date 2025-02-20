fd1f0023fd598c8265e2fcd7c798f114057780cf
Md. Sadiqur Rahman <sadiqur057@gmail.com>
2025-02-21 01:37:57 +0600

    handled middleware to secured protected routes

--- src/middleware.js
+++ src/middleware.js
@@ -2,24 +2,30 @@ import { NextResponse } from "next/server";
 
 export async function middleware(req) {
   const token = req.cookies.get("ebazar")?.value;
+  const adminToken = req.cookies.get("e_bazar")?.value;
   const { pathname } = req.nextUrl;
 
-  if (["/dashboard"].includes(pathname) || pathname.startsWith("/dashboard/")) {
-    if (!token) {
+  if (pathname.startsWith("/dashboard/admin/")) {
+    if (!adminToken) {
       return NextResponse.redirect(new URL("/login", req.url));
     }
     return NextResponse.next();
   }
 
-  if (token && ["/login", "/register"].includes(pathname)) {
+  if (pathname.startsWith("/dashboard")) {
+    if (!adminToken && !token) {
+      return NextResponse.redirect(new URL("/login", req.url));
+    }
+    return NextResponse.next();
+  }
+
+  if ((token || adminToken) && ["/login", "/register"].includes(pathname)) {
     return NextResponse.redirect(new URL("/", req.url));
   }
+
   return NextResponse.next();
 }
 
 export const config = {
-  matcher: [
-    "/(login|register|dashboard)",
-    "/dashboard/:path*",
-  ],
+  matcher: ["/login", "/register", "/dashboard/:path*"],
 };
