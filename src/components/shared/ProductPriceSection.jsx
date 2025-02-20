53918107c87b4e7ace592eee9c2ea662d3f1d823
Md. Sadiqur Rahman <sadiqur057@gmail.com>
2025-02-21 01:50:23 +0600

    added price conversion

---
+++ src/components/shared/ProductPriceSection.jsx
@@ -0,0 +1,15 @@
+"use client";
+import useFetchCurrencyData from "@/hooks/useFetchCurrencyData";
+import React from "react";
+
+const ProductPriceSection = ({ price }) => {
+  const { symbol, conversionRate } = useFetchCurrencyData();
+  return (
+    <p className="text-green-600 font-semibold mb-4">
+      {symbol}
+      &nbsp;{(price * conversionRate).toFixed(2)}
+    </p>
+  );
+};
+
+export default ProductPriceSection;
