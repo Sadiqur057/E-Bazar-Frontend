2218bf0d97c750caec5265ee3a366694e1c727fb
Md. Sadiqur Rahman <sadiqur057@gmail.com>
2025-02-21 01:50:10 +0600

    added api integration

--- src/components/shared/ProductCard.jsx
+++ src/components/shared/ProductCard.jsx
@@ -1,23 +1,34 @@
-import Image from "next/image";
 import React from "react";
 import ProductSectionCart from "./ProductSectionCart";
+import ImageComponent from "./ImageComponent";
+
+import { Star } from "lucide-react";
+import ViewProduct from "./ViewProduct";
+import ProductPriceSection from "./ProductPriceSection";
 
 const ProductCard = ({ product }) => {
   return (
     <div className="border border-gray-200/80 shadow-sm rounded-xl hover:scale-[1.02] scroll-smooth">
-      <Image
-        src={product.image || "/placeholder.svg"}
-        alt={product.name}
-        width={200}
-        height={200}
-        className="w-full p-3 h-48 object-contain object-center"
-      />
-      <div className="p-4">
-        <h3 className="font-semibold text-lg mb-1">{product.name}</h3>
-        <p className="text-green-600 font-bold mb-2">
-          ${product.price.toFixed(2)}
+      <div className="relative">
+        <ImageComponent
+          src={product?.image}
+          alt={product?.name}
+          width={200}
+          height={200}
+          classNames="p-3 w-full h-48 object-contain object-center mx-auto"
+        />
+        <p className="absolute bottom-3 right-4 flex gap-2 items-center bg-white shadow-md p-1 rounded-md">
+          <Star className="fill-orange-400 text-orange-400 w-5" />
+          <span className="font-semibold text-primary">5.0</span>
         </p>
-        <ProductSectionCart />
+      </div>
+      <div className="p-4">
+        <h3 className="font-semibold text-lg mb-1">{product?.name}</h3>
+        <ProductPriceSection price={product?.price} />
+        <div className="flex items-center gap-3">
+          <ProductSectionCart product={product}/>
+          <ViewProduct product={product} />
+        </div>
       </div>
     </div>
   );
