61507f6406d13e21238925b9f61711d089dc3738
Md. Sadiqur Rahman <sadiqur057@gmail.com>
2025-02-21 01:43:57 +0600

    added api integration in shop page

--- src/app/(main-layout)/shop/page.js
+++ src/app/(main-layout)/shop/page.js
@@ -1,8 +1,11 @@
 import Shop from "@/components/shop/Shop";
 import React from "react";
 
-const page = () => {
-  return <Shop />;
+const page = async () => {
+  const url = `${process.env.NEXT_PUBLIC_API_URL}/product`;
+  const res = await fetch(url, { cache: "no-cache" });
+  const products = await res.json();
+  return <Shop products={products?.data} />;
 };
 
 export default page;
--- src/components/shop/Shop.jsx
+++ src/components/shop/Shop.jsx
@@ -1,65 +1,8 @@
 import ProductCard from "../shared/ProductCard";
-import fruit from "@/assets/images/apple.png";
-import egg from "@/assets/images/egg.png";
-import honey from "@/assets/images/honey.png";
-import avocado from "@/assets/images/avocado.png";
 import { ShopHeader } from "./ShopHeader";
 import { ShopSidebar } from "./ShopSidebar";
 
-const products = [
-  {
-    name: "Organic Apples",
-    price: 3.99,
-    image: fruit,
-    description: "Fresh, crisp organic apples from local orchards.",
-  },
-  {
-    name: "Organic Honey",
-    price: 7.99,
-    image: honey,
-    description: "Pure, unprocessed organic honey from local beekeepers.",
-  },
-  {
-    name: "Organic Avocados",
-    price: 4.99,
-    image: avocado,
-    description: "Creamy, ripe organic avocados for your healthy recipes.",
-  },
-  {
-    name: "Free-Range Eggs",
-    price: 5.49,
-    image: egg,
-    description: "Ethically sourced free-range eggs from happy hens.",
-  },
-
-  {
-    name: "Organic Spinach",
-    price: 2.99,
-    image: honey,
-    description:
-      "Nutrient-rich organic spinach, perfect for salads and cooking.",
-  },
-  {
-    name: "Grass-Fed Beef",
-    price: 12.99,
-    image: fruit,
-    description: "Premium grass-fed beef from sustainable farms.",
-  },
-  {
-    name: "Organic Apples",
-    price: 3.99,
-    image: fruit,
-    description: "Fresh, crisp organic apples from local orchards.",
-  },
-  {
-    name: "Organic Honey",
-    price: 7.99,
-    image: honey,
-    description: "Pure, unprocessed organic honey from local beekeepers.",
-  },
-];
-
-const Shop = () => {
+const Shop = ({ products }) => {
   return (
     <div className="container pb-8">
       <div className="lg:grid lg:grid-cols-[240px,1fr] gap-10">
@@ -67,7 +10,7 @@ const Shop = () => {
         <div>
           <ShopHeader />
           <div className="grid gap-6 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
-            {products.map((product, index) => (
+            {products?.map((product, index) => (
               <ProductCard key={index} product={product} />
             ))}
           </div>
